1290bc19a45a90a921420f50edf7c7c121d23657
Fatima20027 <fatima.hailou@icloud.com>
2024-03-26 15:16:52 +0000

    betty fixed in p_aall_l.c

--- p_aall_l.c
+++ p_aall_l.c
@@ -1,6 +1,12 @@
 #include "monty.h"
 
-void pall(stack_t **heaa__d , unsigned int valu__ee)
+/**
+ * pall - prints all the values on the stack, starting from the top.
+ * @heaa__d: double pointer to the stack's head.
+ * @valu__ee: Value of the opcode in the Monty file.
+ */
+
+void pall(stack_t **heaa__d, unsigned int valu__ee)
 {
 	stack_t *heaa__d = *heaa__d;
 	(void)valu__ee;
