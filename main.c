fd48a1ab13c33d332ea8399259ed76cbf4a449f1
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-28 11:43:02 +0000

    task 0?

--- main.c
+++ main.c
@@ -10,79 +10,34 @@
 
 int main(int argc, char **argv)
 {
-	const char *filename;
-	int resu__ullt;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
-
-	filename = argv[1];
-	resu__ullt = process_file(filename);
-
-	return (resu__ullt);
-}
-/**
- * process - Process file
- * @filename: Name of the file to process
- *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
- */
-
-int process_file(const char *filename)
-{
-	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char *line_copy;
-	int result;
-	unsigned int lens = 0;
+	int fd = 0, read_bytes = 0, len = 0, valid = 0;
+	unsigned int line = 0;
+	char *buffer = NULL;
 	stack_t *head = NULL;
 
-	file = fopen(filename, "r");
-	if (file == NULL)
+	if (ac != 2)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", filename);
-		return (EXIT_FAILURE);
+		_puts("USAGE: monty file\n", 2);
+		exit(EXIT_FAILURE);
 	}
-
-	while ((read = read_geet_linee(&line, &len, file)) != -1)
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
 	{
-		lens++;
-		line_copy = _strdup(line);
-		if (line_copy == NULL)
-		{
-			fprintf(stderr, "Error: Memory allocation failed\n");
-			fre__ee(&head);
-			fclose(file);
-			free(line);
-			return (EXIT_FAILURE);
-		}
-
-		if (line_copy[0] != '#')
-		{
-			result = exec__cuu__uuutee(&head, line_copy, lens);
-		}
-
-		if (result == -1)
-		{
-			fprintf(stderr, "L%d: Unknown instruction %s", lens, line_copy);
-			free(line_copy);
-			fre__ee(&head);
-			fclose(file);
-			free(line);
-			return (EXIT_FAILURE);
-		}
-
-		free(line_copy);
+		_puts("Error: Can't open file ", 2);
+		_puts(av[1], 2);
+		_putchar('\n', 2);
+		exit(EXIT_FAILURE);
 	}
-
-	fre__ee(&head);
-	fclose(file);
-	free(line);
-
-	return (EXIT_SUCCESS);
+	while ((read_bytes = _getline(&buffer, &len, fd, &line)) != -1)
+	{
+		valid = is_buffer_valid(buffer);
+		if (valid == -1)
+			continue;
+		check_cmd(buffer, line, &head);
+	}
+	if (buffer)
+		free(buffer);
+	free_stack(head);
+	close(fd);
+	return (0);
 }
