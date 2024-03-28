efecc4d755734f3bdab3b0271200c5dd3eeccce1
Fatima20027 <fatima.hailou@icloud.com>
2024-03-28 15:00:50 +0000

    betty was fixed

--- fre_e_ee.c
+++ fre_e_ee.c
@@ -1,13 +1,19 @@
 #include "monty.h"
+/**
+ * free_stackee - Frees a stack.
+ * @heea__add: Pointer to the top of the stack.
+ */
 
-void fre__ee(stack_t **hea__add)
+void free_stackee(stack_tt **heea__add)
 {
-	stack_t *tmp = *hea__add;
+	stack_tt *tmp = *heea__add;
 
-	while (tmp!= NULL) {
-		stack_t *tmp2 = tmp->next;
+	while (tmp != NULL)
+	{
+		stack_tt *tmp2 = tmp->next;
+
 		free(tmp);
 		tmp = tmp2;
 	}
-	*hea__add = NULL;
+	*heea__add = NULL;
 }
