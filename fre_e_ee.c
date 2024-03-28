60e1d85b90604f796284665f8e53fa162f4a2462
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-28 10:55:48 +0000

    logic of free implemented

--- fre_e_ee.c
+++ fre_e_ee.c
@@ -1,1 +1,13 @@
 #include "monty.h"
+
+void fre__ee(stack_t **hea__add)
+{
+	stack_t *tmp = *hea__add;
+
+	while (tmp!= NULL) {
+		stack_t *tmp2 = tmp->next;
+		free(tmp);
+		tmp = tmp2;
+	}
+	*hea__add = NULL;
+}
